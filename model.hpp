5b354216ece03db776779fee11ef2aee5d0a0c5d
ali <a@topost.net>
2023-10-28 19:49:33 +1000

    model

--- model.hpp
+++ model.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
-#undef PACKETIZER_USE_CRC_AS_DEFAULT
-#include <MsgPack.h>
+// max number of decoded packet queues
+#define PACKETIZER_MAX_PACKET_QUEUE_SIZE 1
+// max data bytes in packet
+#define PACKETIZER_MAX_PACKET_BINARY_SIZE 128
+// max number of callback for one stream
+#define PACKETIZER_MAX_CALLBACK_QUEUE_SIZE 4
+// max number of streams
+#define PACKETIZER_MAX_STREAM_MAP_SIZE 2
 
 // max publishing elemtnt size in one destination
 // #define MSGPACKETIZER_MAX_PUBLISH_ELEMENT_SIZE 64
@@ -46,51 +52,14 @@
 
 typedef uint8_t chan_id;
 
-struct Channel {
-  chan_id id;
-};
 
-
-enum led_command_type : uint8_t {
-  LED_START,
-  LED_STOP,
-  LED_MODE_PARTY,
-  LED_MODE_RAINBOW,
-  LED_MODE_COLOUR,
-};
-
-typedef uint8_t LED_COMMAND;
-
-struct error {
-  STRINGT message;
-  MSGPACK_DEFINE(message);
-};
-
-struct inform_channel {
-  chan_id channel;
-  uint8_t light_pin;
-  uint8_t sensor_pin;
-  MSGPACK_DEFINE(channel, light_pin, sensor_pin);
-};
-
-struct led {
-  chan_id channel;
-  LED_COMMAND command;
-  bool fade;
-  MSGPACK_DEFINE(channel, command, fade);
-};
-
-struct led_update {
+struct __attribute__ ((packed)) led_update {
   uint8_t led;
-  MSGPACK_DEFINE(led);
-  //MsgPack::bin_t<uint8_t> lights;
 };
 
 
-struct touch_event {
+struct  __attribute__ ((packed)) touch_event {
   uint8_t state;
-  MSGPACK_DEFINE(state);
-
 
   int size() const {
     return 8;
@@ -99,14 +68,14 @@ struct touch_event {
   bool is_active(uint8_t pin) const {
     return (1 << pin) & state;
   }
-
 };
 
+#define MAXNUM_LEDS 10
 
-struct lighting_update {
+struct __attribute__ ((packed)) lighting_update {
   chan_id channel;
-  MsgPack::bin_t<uint8_t> lights;
-  MSGPACK_DEFINE(channel, lights);
+  uint8_t num_leds = MAXNUM_LEDS;
+  uint8_t lights[MAXNUM_LEDS];
 };
 
 
@@ -120,10 +89,10 @@ namespace message {
     LED_UPDATE = 0x64
   };
 
-  typedef error ERROR_t;
-  typedef led LED_t;
+ // typedef error ERROR_t;
+ // typedef led LED_t;
   typedef touch_event TOUCH_EVENT_t;
-  typedef inform_channel INFORM_CHANNEL_t;
+//  typedef inform_channel INFORM_CHANNEL_t;
   typedef lighting_update LIGHTING_UPDATE_t;
   typedef led_update LED_UPDATE_t;
 };
